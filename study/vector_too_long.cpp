fc54e4abc9a9be8524ff52338b11329c3c8b830b
sz <698307+xparq@users.noreply.github.com>
2023-08-31 11:51:04 +0200

    Cornered the "vector too long" error. Wow. C++ is evil.

---
+++ study/vector_too_long.cpp
@@ -0,0 +1,23 @@
+﻿/*****************************************************************************
+  The amazing "vector too long" error for `auto v = vector<X>{"a", "b"}`.
+  Both in MSVC and GCC. No warning even with -Wall.
+  https://stackoverflow.com/questions/77011695/haunted-stdvector-construction
+ *****************************************************************************/
+
+#include <vector>
+#include <exception>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try { 
+        struct X {
+            X(string) {}       // Just adding this to confuse you even more!... ;)
+            explicit X(int) {} // But this one's still a mystery: why doesn't the `explicit` stop it?
+        };
+        vector<X>("a", "b");
+    } catch (exception& x) {
+        cerr << x.what();
+    }
+}
